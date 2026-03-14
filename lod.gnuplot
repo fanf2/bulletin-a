45c49d6a05a11157c3cf58fc8efe0c8a81410c1c
Tony Finch <dot@dotat.at>
2026-03-14 21:35:53 +0000

    plot LoD and UT1-UTC from earth orientation parameters

---
+++ lod.gnuplot
@@ -0,0 +1,20 @@
+set term qt size 2000,1000
+set grid dashtype ". "
+set linetype 1 linewidth 2
+set linetype 2 linewidth 2
+
+set xdata time
+set timefmt "%Y-%m-%d"
+set xrange ["2006-06-01":"2026-06-01"]
+set xtics format "%Y-%m-%d" time
+set xtics 12 months
+set mxtics 4
+set ytics 100
+
+# ./lod.pl <finals2000A.all.json >lod
+
+plot 'lod' using 1:2 with lines title "UT1-UTC" \
+   , ''    using 1:3 with lines title "LoD" \
+   , 0               with lines notitle
+
+pause mouse close
---
+++ lod.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use v5.30;
+
+use Data::Dumper;
+use JSON;
+use List::Util qw{ sum };
+
+# metadata https://www.iers.org/IERS/EN/DataProducts/EarthOrientationData/eop
+# eg https://datacenter.iers.org/data/json/finals2000A.data.json
+# eg https://datacenter.iers.org/data/json/finals2000A.all.json
+
+# slurp
+undef $/;
+my $eop = decode_json <STDIN>;
+
+my @tk = qw{ dateYear dateMonth dateDay };
+my @dk = qw{ UT1-UTC LOD };
+
+my @lodm;
+my @lodmm;
+my @lody;
+my @lodyy;
+
+for my $i (@{ $eop->{EOP}->{data}->{timeSeries} }) {
+	my $t = $i->{time};
+	my $date = join '-', $t->@{@tk};
+	my $lod = $i->{dataEOP}->{UT}->[0]->{LOD};
+	my $dut = $i->{dataEOP}->{UT}->[0]->{'UT1-UTC'};
+	last if $lod eq '';
+	$lod *= 1000;
+	$dut *= 1000;
+	push @lodm,  $lod; shift @lodm  if @lodm  > 30;
+	push @lodmm, $lod; shift @lodmm if @lodmm > 30 * 3;
+	push @lody,  $lod; shift @lody  if @lody  > 365;
+	push @lodyy, $lod; shift @lodyy if @lodyy > 365 * 4;
+	printf "%s %+4.0f %+5.0f %+5.0f\n",
+	    $date, $dut,
+	    # $lod,
+	    # sum(@lodm) / @lodm,
+	    # sum(@lodmm) / @lodmm,
+	    sum(@lody) / @lody,
+	    sum(@lodyy) / @lodyy
+	    if $t->{dateDay} eq '01';
+}
+
+say "#   date   dutc   lod   lod";
+say "#            ms    µs    µs";
+say "#                  1y    4y";
