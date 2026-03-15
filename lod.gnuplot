9716e1735fc92ef5d8c40ee07fd2554e631cb118
Tony Finch <dot@dotat.at>
2026-03-15 18:10:16 +0000

    plot dX and dY as well

--- lod.gnuplot
+++ lod.gnuplot
@@ -1,20 +1,22 @@
 set term qt size 2000,1000
 set grid dashtype ". "
-set linetype 1 linewidth 2
-set linetype 2 linewidth 2
 
 set xdata time
 set timefmt "%Y-%m-%d"
-set xrange ["2006-06-01":"2026-06-01"]
 set xtics format "%Y-%m-%d" time
 set xtics 12 months
-set mxtics 4
+set mxtics 12
 set ytics 100
 
-# ./lod.pl <finals2000A.all.json >lod
+set linetype 1 linewidth 2
+set linetype 2 linewidth 2
+set linetype 5 linecolor "black"
 
-plot 'lod' using 1:2 with lines title "UT1-UTC" \
-   , ''    using 1:6 with lines title "LoD" \
-   , 0               with lines notitle
+plot '< ./dxdy.pl <finals2000A.data.json' \
+    using 1:2 with lines title "UT1-UTC" ,''\
+    using 1:5 with lines title "LoD"     ,''\
+    using 1:6 with lines title "dX"      ,''\
+    using 1:7 with lines title "dY"      ,''\
+    eq''? 0:0 with lines notitle
 
 pause mouse close
--- lod.pl
+++ lod.pl
@@ -4,7 +4,6 @@ use warnings;
 use strict;
 use v5.30;
 
-use Data::Dumper;
 use JSON;
 use List::Util qw{ sum };
 
@@ -17,35 +16,35 @@ undef $/;
 my $eop = decode_json <STDIN>;
 
 my @tk = qw{ dateYear dateMonth dateDay };
-my @dk = qw{ UT1-UTC LOD };
+
+my $bulA = 0;
+my $bulB = 1;
 
 my @lodm;
-my @lodmm;
 my @lody;
-my @lodyy;
 
 for my $i (@{ $eop->{EOP}->{data}->{timeSeries} }) {
 	my $t = $i->{time};
 	my $date = join '-', $t->@{@tk};
-	my $lod = $i->{dataEOP}->{UT}->[0]->{LOD};
-	my $dut = $i->{dataEOP}->{UT}->[0]->{'UT1-UTC'};
-	last if $lod eq '';
+
+	my $lod = $i->{dataEOP}->{UT}->[$bulA]->{LOD};
+	my $dut = $i->{dataEOP}->{UT}->[$bulA]->{'UT1-UTC'};
+	my $dX = $i->{dataEOP}->{nutation}->[$bulA]->{dX};
+	my $dY = $i->{dataEOP}->{nutation}->[$bulA]->{dY};
+	last unless $lod and $dut and $dX and $dY;
+
 	$lod *= 1000;
 	$dut *= 1000;
-	push @lodm,  $lod; shift @lodm  if @lodm  > 30;
-	push @lodmm, $lod; shift @lodmm if @lodmm > 30 * 3;
-	push @lody,  $lod; shift @lody  if @lody  > 365;
-	push @lodyy, $lod; shift @lodyy if @lodyy > 365 * 4;
-	printf "%s %+4.0f %+5.0f %+5.0f %+5.0f %+5.0f %+5.0f\n",
-	    $date, $dut,
-	    $lod,
-	    sum(@lodm) / @lodm,
-	    sum(@lodmm) / @lodmm,
-	    sum(@lody) / @lody,
-	    sum(@lodyy) / @lodyy
-	    if $t->{dateDay} eq '01';
-}
+	$dX *= 1000;
+	$dY *= 1000;
 
-say "#   date   dutc   lod   lod   lod   lod   lod";
-say "#            ms    µs    µs    µs    µs    µs";
-say "#                 day   1mo   4mo    1y    4y";
+	push @lodm, $lod; shift @lodm if @lodm > 30;
+	push @lody, $lod; shift @lody if @lody > 300;
+
+	printf "%s %+4.0f %+5.0f %+5.0f %+5.0f %+5.0f %+5.0f\n",
+	    $date, $dut, $lod,
+	    sum(@lodm) / @lodm,
+	    sum(@lody) / @lody,
+	    $dX, $dY
+	    if $date gt '2006-06-01';
+}
